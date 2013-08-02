11575d9363bb55f3d5d1c529b9b79452d1be42ca
furuya02 <sin@sapporoworks.ne.jp>
2013-08-03 07:44:47 +0900

    2013.08.03 0745

--- ChangeVersion.js
+++ ChangeVersion.js
@@ -1,7 +1,7 @@
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 
 //バージョン
-var version = "5.9.2.0";
+var version = "5.9.3.0";
 //ソースコードのフォルダ
 //var srcDir = fso.GetFolder("C:\\tmp2\\bjd5");
 var currentDir = WScript.CreateObject("WScript.Shell").CurrentDirectory;
--- ProxyPop3Server/Properties/AssemblyInfo.cs
+++ ProxyPop3Server/Properties/AssemblyInfo.cs
@@ -34,8 +34,10 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.9.2.0")]
-[assembly: AssemblyFileVersion("5.9.2.0")]
+[assembly: AssemblyVersion("5.9.3.0")]
+[assembly: AssemblyFileVersion("5.9.3.0")]
+
+
 
 
 
