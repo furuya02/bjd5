ff77175bbae3554362f329ea41542184f662892c
furuya02 <sin@sapporoworks.ne.jp>
2013-11-20 05:30:26 +0900

    2013.11.20 0530

--- SipServer/Properties/AssemblyInfo.cs
+++ SipServer/Properties/AssemblyInfo.cs
@@ -35,8 +35,9 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.9.7.0")]
-[assembly: AssemblyFileVersion("5.9.7.0")]
+[assembly: AssemblyVersion("5.9.8.0")]
+[assembly: AssemblyFileVersion("5.9.8.0")]
+
 
 
 
