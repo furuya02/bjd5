63d154d014cadd703fb530024d4275059460b468
furuya02 <sin@sapporoworks.ne.jp>
2013-12-22 12:36:53 +0900

    2013.12.22 1230 Ver6.0.0

--- BJD/Properties/AssemblyInfo.cs
+++ BJD/Properties/AssemblyInfo.cs
@@ -28,10 +28,11 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.9.9.0")]
-[assembly: AssemblyFileVersion("5.9.9.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestBJD")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("BJDTestProject")]
+
 
 
 
--- Pop3Server/Properties/AssemblyInfo.cs
+++ Pop3Server/Properties/AssemblyInfo.cs
@@ -118,3 +118,4 @@ using System.Runtime.InteropServices;
 
 
 
+
