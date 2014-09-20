66d94ee307b070963dbe8d9dcf8841fb30fce464
furuys02 <sin@sapporoworks.ne.jp>
2014-09-20 13:47:28 +0900

    2014.09.20 1350

--- SampleServer/Properties/AssemblyInfo.cs
+++ SampleServer/Properties/AssemblyInfo.cs
@@ -35,8 +35,9 @@ using System.Runtime.CompilerServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.7")]
-[assembly: AssemblyFileVersion("6.0.7")]
+[assembly: AssemblyVersion("6.0.8")]
+[assembly: AssemblyFileVersion("6.0.8")]
+
 
 
 
