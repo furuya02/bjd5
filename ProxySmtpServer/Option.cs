53c2e3bf4989db211c99aa5c7cae1f634ef980f6
SIN <sin@sapporoworks.ne.jp>
2015-03-24 13:32:41 +0900

    2015.03.24 1330

--- ProxySmtpServer/Option.cs
+++ ProxySmtpServer/Option.cs
@@ -14,11 +14,14 @@ namespace ProxySmtpServer {
         public Option(Kernel kernel, string path, string nameTag)
             : base(kernel.IsJp(), path, nameTag) {
 
-            Add(new OneVal("useServer", false, Crlf.Nextline, new CtrlCheckBox(IsJp() ? "SMTPプロキシを使用する" : "Use SMTP Proxy")));
+                var key = "useServer";
+            Add(new OneVal(key, false, Crlf.Nextline, new CtrlCheckBox(Lang.Value(key))));
 
             var pageList = new List<OnePage>();
-            pageList.Add(Page1("Basic", IsJp() ? "基本設定" : "Basic", kernel));
-            pageList.Add(Page2("Expansion", IsJp() ? "拡張設定" : "Expansion", kernel));
+            key = "Basic";
+            pageList.Add(Page1(key, Lang.Value(key), kernel));
+            key = "Expansion";
+            pageList.Add(Page2(key, Lang.Value(key), kernel));
             pageList.Add(PageAcl());
             Add(new OneVal("tab", null, Crlf.Nextline, new CtrlTabPage("tabPage", pageList)));
 
@@ -29,30 +32,28 @@ namespace ProxySmtpServer {
             var onePage = new OnePage(name, title);
 
             onePage.Add(CreateServerOption(ProtocolKind.Tcp, 8025, 60, 10)); //サーバ基本設定
-
-            onePage.Add(new OneVal("targetPort", 25, Crlf.Nextline, new CtrlInt(IsJp() ? "接続先ポート" : "Port", 5)));
-            onePage.Add(new OneVal("targetServer", "", Crlf.Nextline, new CtrlTextBox(IsJp() ? "接続先サーバ" : "Server", 50)));
-            onePage.Add(new OneVal("idleTime", 1, Crlf.Nextline, new CtrlInt(IsJp() ? "アイドルタイム(m)" : "Idle time (m)", 5)));
+            var key = "targetPort";
+            onePage.Add(new OneVal(key, 25, Crlf.Nextline, new CtrlInt(Lang.Value(key), 5)));
+            key = "targetServer";
+            onePage.Add(new OneVal(key, "", Crlf.Nextline, new CtrlTextBox(Lang.Value(key), 50)));
+            key = "idleTime";
+            onePage.Add(new OneVal(key, 1, Crlf.Nextline, new CtrlInt(Lang.Value(key), 5)));
 
             return onePage;
         }
         private OnePage Page2(string name, string title, Kernel kernel){
             var onePage = new OnePage(name, title);
             var l = new ListVal();
-            l.Add(new OneVal("mail", "", Crlf.Nextline,
-                             new CtrlTextBox(
-                                 IsJp()
-                                     ? "メールアドレス（メールクライアントで設定したもの）"
-                                     : "MailAddress(The thing which I set in an email client)", 30)));
-            l.Add(new OneVal("server", "", Crlf.Contonie, new CtrlTextBox(IsJp() ? "接続先サーバ" : "Server", 30)));
-            l.Add(new OneVal("dstPort", 25, Crlf.Nextline, new CtrlInt(IsJp() ? "接続先ポート" : "Port", 5)));
-            l.Add(new OneVal("address", "", Crlf.Nextline,
-                             new CtrlTextBox(
-                                 IsJp()
-                                     ? "メールアドレス（プロパイダで指定されたもの）"
-                                     : "MainAddress(The thing which was appointed in a supplier)", 30)));
-            onePage.Add(new OneVal("specialUser", null, Crlf.Nextline,
-                                   new CtrlDat(IsJp() ? "特別なユーザの指定" : "Special User", l, 360, IsJp())));
+            var key = "mail";
+            l.Add(new OneVal(key, "", Crlf.Nextline,new CtrlTextBox(Lang.Value(key), 30)));
+            key = "server";
+            l.Add(new OneVal(key, "", Crlf.Contonie, new CtrlTextBox(Lang.Value(key), 30)));
+            key = "dstPort";
+            l.Add(new OneVal(key, 25, Crlf.Nextline, new CtrlInt(Lang.Value(key), 5)));
+            key = "address";
+            l.Add(new OneVal(key, "", Crlf.Nextline,new CtrlTextBox(Lang.Value(key), 30)));
+            key = "specialUser";
+            onePage.Add(new OneVal(key, null, Crlf.Nextline,new CtrlDat(Lang.Value(key), l, 360, IsJp())));
 
             return onePage;
         }
--- ProxyTelnetServer/Option.cs
+++ ProxyTelnetServer/Option.cs
@@ -14,11 +14,11 @@ namespace ProxyTelnetServer {
 
         public Option(Kernel kernel, string path, string nameTag)
             : base(kernel.IsJp(), path, nameTag) {
-
-            Add(new OneVal("useServer", false, Crlf.Nextline, new CtrlCheckBox(IsJp() ? "プロキシサーバ[Telnet]を使用する" : "Use Proxy Server [Telnet]")));
-
+                var key = "useServer";
+                Add(new OneVal(key, false, Crlf.Nextline, new CtrlCheckBox(Lang.Value(key))));
             var pageList = new List<OnePage>();
-            pageList.Add(Page1("Basic", IsJp() ? "基本設定" : "Basic", kernel));
+            key = "Basic";
+            pageList.Add(Page1(key, Lang.Value(key), kernel));
             pageList.Add(PageAcl());
             Add(new OneVal("tab", null, Crlf.Nextline, new CtrlTabPage("tabPage", pageList)));
 
@@ -30,7 +30,8 @@ namespace ProxyTelnetServer {
 
             onePage.Add(CreateServerOption(ProtocolKind.Tcp, 8023, 60, 10)); //サーバ基本設定
 
-            onePage.Add(new OneVal("idleTime", 1, Crlf.Contonie, new CtrlInt(IsJp() ? "アイドルタイム(m)" : "Idle Timeout(sec)", 5)));
+            var key = "";
+            onePage.Add(new OneVal(key, 1, Crlf.Contonie, new CtrlInt(Lang.Value(key), 5)));
 
 
             return onePage;
