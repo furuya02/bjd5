4d0250e192cde71bb4e1929bdf5f757987b72a12
SIN <sin@sapporoworks.ne.jp>
2015-03-29 13:45:06 +0900

    2015.03.29 0040

--- RemoteServer/Log.cs
+++ RemoteServer/Log.cs
@@ -1,20 +1,27 @@
 
 namespace RemoteServer {
     partial class Server {
+        //BJD.Lang.txtに必要な定義が揃っているかどうかの確認
         protected override void CheckLang()
         {
+            for (var n = 1; n <= 5; n++)
+            {
+                Lang.Value(n);
+            }
         }
 
-        public override string GetMsg(int messageNo) {
-            switch (messageNo) {
-                case 1: return (Kernel.IsJp())?"リモートクライアントから接続されました":"Connected by a RemoteClient";
-                case 2: return (Kernel.IsJp())?"リモートクライアントからの接続が解除されました":"Disconnected by a RemoteClient";
-                case 3: return (Kernel.IsJp())?"リモートサーバスレッドが初期化されていません":"A RemoteServerThread is not initialized";
-                case 4: return (Kernel.IsJp())?"パスワードが違います":"Password incorrect";
-                case 5: return (Kernel.IsJp())?"認証パスワードは設定されていません":"No password";
+        public override string GetMsg(int messageNo)
+        {
+            switch (messageNo)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    return Lang.Value(messageNo);
             }
             return "unknown";
         }
-
     }
 }
