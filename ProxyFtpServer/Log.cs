edc22869e359158a8d084beeb39721e268536081
SIN <sin@sapporoworks.ne.jp>
2015-03-29 13:19:15 +0900

    2-15.03.29 0030

--- ProxyFtpServer/Log.cs
+++ ProxyFtpServer/Log.cs
@@ -1,24 +1,30 @@
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace ProxyFtpServer {
     partial class Server {
+
+        //BJD.Lang.txtに必要な定義が揃っているかどうかの確認
         protected override void CheckLang()
         {
+            for (var n = 1; n <= 8; n++)
+            {
+                Lang.Value(n);
+            }
         }
-
         public override string GetMsg(int messageNo) {
             switch (messageNo) {
-                case 1: return Kernel.IsJp()?"認証に失敗しました":"Failed in the certification";
-                case 2: return Kernel.IsJp()?"アイドルタイムアウト":"An idle time out";
-                case 3: return Kernel.IsJp()?"送信に失敗しました":"Transmission of a message failure";
-                case 4: return Kernel.IsJp()?"PORTコマンドの解釈に失敗しました":"Interpretation failure of a command(PORT)";
-                case 5: return Kernel.IsJp()?"PASVコマンドの解釈に失敗しました(1)":"Interpretation failure of a command(PASV)[1]";
-                case 6: return Kernel.IsJp()?"PASVコマンドの解釈に失敗しました(2)":"Interpretation failure of a command(PASV)[2]";
-                case 7: return Kernel.IsJp()?"コマンド受信":"The command reception";
-                case 8: return Kernel.IsJp() ? "「USER ユーザ名@ホスト名」の接続形式しか対応できません。" : "Only a connection form of [USER username@hostname] can support.";
-
+                case 1: 
+                case 2: 
+                case 3: 
+                case 4: 
+                case 5: 
+                case 6: 
+                case 7: 
+                case 8:
+                    return Lang.Value(messageNo);
             }
             return "unknown";
         }
-
     }
 }
