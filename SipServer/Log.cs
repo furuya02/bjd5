00a227e45625cb47a5abd8bb331ff1af77a6bcae
SIN <sin@sapporoworks.ne.jp>
2015-03-29 14:17:37 +0900

    2015.03.29 0200

--- SipServer/Log.cs
+++ SipServer/Log.cs
@@ -1,14 +1,18 @@
 ﻿
 namespace SipServer {
     partial class Server {
+        //BJD.Lang.txtに必要な定義が揃っているかどうかの確認
         protected override void CheckLang()
         {
+            Lang.Value(1);
         }
 
-        public override string GetMsg(int messageNo) {
-
-            switch (messageNo) {
-                case 1: return Kernel.IsJp() ? "日本語" : "English";//この形式でログ用のメッセージ追加できます。
+        public override string GetMsg(int messageNo)
+        {
+            switch (messageNo)
+            {
+                case 1:
+                    return Lang.Value(messageNo);
             }
             return "unknown";
         }
